8ebe0ab3c90bb5cd3bf6f239892646a473f0de9d
David Southgate <davidsouthgate97@gmail.com>
2018-04-26 10:58:24 +0000

    Update gitlab-markdown-dont-navigate-away.user.js

--- gitlab-markdown-dont-navigate-away.user.js
+++ gitlab-markdown-dont-navigate-away.user.js
@@ -3,11 +3,12 @@
 // @namespace    https://gitlab.com/southgate/userscripts/gitlab-markdown-dont-navigate-away/raw/master/gitlab-markdown-dont-navigate-away.user.js
 // @updateURL    https://gitlab.com/southgate/userscripts/gitlab-markdown-dont-navigate-away/raw/master/gitlab-markdown-dont-navigate-away.user.js
 // @downloadURL  https://gitlab.com/southgate/userscripts/gitlab-markdown-dont-navigate-away/raw/master/gitlab-markdown-dont-navigate-away.user.js
-// @version      0.1.1
+// @version      0.1.2
 // @description  Don't navigate away when editing gitlab markdown. Saves loss of work!
 // @author       David Southgate
 // @licence      MIT; https://gitlab.com/southgate/userscripts/gitlab-markdown-dont-navigate-away/raw/master/LICENSE
-// @match        https://gitlab.com/*/edit/*.md
+// @match        *://gitlab.*/*/edit/*.md
+// @match        *://130.209.251.84/*/edit/*.md
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
 // @grant        none
 // ==/UserScript==
